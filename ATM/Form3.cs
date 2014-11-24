a2f81047f5fa33079655ad3761999b759e616e1f
Zhibo Qi <qzb0015@gmail.com>
2014-11-24 14:33:15 +0800

    Add-1
    Add the program ATM build by vs2010

---
+++ ATM/Form3.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.OleDb;
+
+namespace ATM
+{
+    public partial class Form3 : Form
+    {
+        public OleDbConnection getConn()
+    {
+        string connstr="Provider=Microsoft.Jet.OLEDB.4.0 ;Data Source=F:\\ATM.accdb";
+        OleDbConnection tempconn= new OleDbConnection(connstr);
+        return(tempconn);
+    }         
+
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //OleDbConnection conn = getConn(); //getConn():得到连接对象
+            //conn.Open();
+
+            //string insertstr = "INSERT INTO ATMTable(Card, Password, Money) VALUES ('";
+            //insertstr += ATMTable.Card + "','";
+            //insertstr += ATMTable.Password + "','";
+            //insertstr += ATMTable.Money + "')";
+
+            //OleDbCommand insertcmd = new OleDbCommand(insertstr, conn);
+            //insertcmd.ExecuteNonQuery();
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
